a888f7aff524a6df68d7e7267b20d083a2fd11a6
Flyukiy <kamoliddin7usmonov@gmail.com>
2021-07-30 15:39:57 +0900

    image.png added, fixed bugs

--- lib/half_moon.dart
+++ lib/half_moon.dart
@@ -37,46 +37,8 @@ class DrawHalfMoon extends CustomPainter {
     );
   }
 
-  //
-  //   final pointA = Offset(0, 0);
-  //   final pointB = Offset(r * sin(72.toRadian()), -r * cos(72.toRadian()));
-  //   final pointC = Offset(r * sin(36.toRadian()), r * cos(36.toRadian()));
-  //   final pointD = Offset(-pointC.dx, pointC.dy);
-  //   final pointE = Offset(-pointB.dx, pointB.dy);
-  //
-  //   final Path path = Path()
-  //     ..moveTo(pointA.dx, pointA.dy)
-  //     ..quadraticBezierTo(pointA.dx, pointA.dy, pointD.dx, pointD.dy)
-  //     ..lineTo(pointA.dx, pointD.dy)
-  //     // ..lineTo(pointE.dx, pointE.dy)
-  //     // ..lineTo(pointB.dx, pointB.dy)
-  //     // ..lineTo(pointD.dx, pointD.dy)
-  //     ..close();
-  //
-  //   final Paint yellowPaint = Paint()
-  //     ..color = Colors.white
-  //     ..style = PaintingStyle.fill;
-  //
-  //   final pointO = Offset(0, 0);
-  //
-  //   final pointI = Offset(width / 2, height / 2);
-  //
-  //   final translationVector = pointI - pointO;
-  //
-  //   final realPath = path.shift(translationVector);
-  //
-  //   // vẽ lại realPath
-  //   canvas.drawPath(realPath, yellowPaint);
-  // }
-
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
     return true;
-  }
-}
-
-extension NumberUtil on num {
-  num toRadian() {
-    return this * pi / 180;
   }
 }
